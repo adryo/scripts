89d9ee8f3d8e111d53079ae4498b037d8933d3e4
Yadir Hernandez <yadirhb@gmail.com>
2018-11-28 09:57:00 -0300

    Added ts script

---
+++ create-ts-lib.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# DESCRIPTION
+# Creates a blank TypeScript library project
+#
+# CREDITS
+# Source  : https://github.com/adryo/scripts
+###############################################################################
+
+echo "Hello world"
