3bc893d419a5c468edcabbb951346b511d28c26c
Yadir Hernandez <yadirhb@gmail.com>
2018-11-28 10:39:29 -0300

    Adding check and installation of jQ

--- create-ts-lib.sh
+++ create-ts-lib.sh
@@ -8,4 +8,28 @@
 # curl https://raw.githubusercontent.com/adryo/scripts/develop/create-ts-lib.sh | bash
 ###############################################################################
 
+
+# Idenfity platform
+PLATFORM=`uname`
+
 echo "Hello $1, how are you?"
+
+readonly NAME="$1"
+
+if [[ "$NAME" != "" ]]; then
+    if ! type jq >/dev/null 2>&1; then
+        echo "'jq' not installed. Trying to install it automatically..."
+        if [[ "$PLATFORM" == 'Linux' ]]; then
+            sudo apt-get install jq
+        else
+            brew install jq
+        if
+    fi
+
+    if ! type jq >/dev/null 2>&1; then
+        echo "Unable to install 'jq'. Exiting..."
+        exit 1
+    fi
+
+    echo "Cloning the repo..."
+fi
