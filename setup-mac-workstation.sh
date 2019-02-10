b4ee1fc44d0e8f004f0a9bc2783de2a8e844d563
adryo <adryoenterprise@gmail.com>
2019-02-10 10:48:44 -0200

    Create setup-mac-workstation.sh

---
+++ setup-mac-workstation.sh
@@ -0,0 +1,20 @@
+
+# Install React stuff
+
+npm install -g expo-cli
+
+brew install watchman
+
+npm install -g react-native-cli
+
+# Install Flutter stuff
+curl https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.0.0-stable.zip -o ~/Downloads/flutter_macos_v1.0.0-stable.zip
+mkdir -p ~/Library/Flutter/ && cd ~/Library/Flutter/
+unzip ~/Downloads/flutter_macos_v1.0.0-stable.zip
+mv ~/Library/Flutter/flutter ~/Library/Flutter/sdk
+echo 'export FLUTTER_HOME="$HOME/Library/Flutter/sdk"' >> ~/.bash_profile
+source ~/.bash_profile
+
+cd ~
+
+flutter doctor
