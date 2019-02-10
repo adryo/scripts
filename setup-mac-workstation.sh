106fcd0a6c2ed7d9705465efb86d658ff33cec2b
adryo <adryoenterprise@gmail.com>
2019-02-10 10:54:24 -0200

    Update setup-mac-workstation.sh

--- setup-mac-workstation.sh
+++ setup-mac-workstation.sh
@@ -13,7 +13,7 @@ mkdir -p ~/Library/Flutter/ && cd ~/Library/Flutter/
 unzip ~/Downloads/flutter_macos_v1.0.0-stable.zip
 mv ~/Library/Flutter/flutter ~/Library/Flutter/sdk
 echo 'export FLUTTER_HOME="$HOME/Library/Flutter/sdk"' >> ~/.bash_profile
-echo 'export PATH="$PATH:$FLUTTER_HOME/bin" >> ~/.bash_profile
+echo 'export PATH="$PATH:$FLUTTER_HOME/bin"' >> ~/.bash_profile
 source ~/.bash_profile
 
 cd ~
