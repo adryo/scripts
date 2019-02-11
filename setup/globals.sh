ca78d25cd1c7aabd6eac6c8c58a88babe65ccc70
adryo <adryoenterprise@gmail.com>
2019-02-11 15:05:27 -0200

    Update globals.sh

--- setup/globals.sh
+++ setup/globals.sh
@@ -4,17 +4,117 @@
 CURRENT_LOGON_PASSWORD=""
 
 # Globals
-GLOBAL_PLATFORM=`uname`
+GLOBAL_PLATFORM_OS="$(uname -s)"
 
-run_expect() {
-  expect -c "set timeout -1; exp_internal 1; spawn $1; expect -re {(P|p)assword*} {send \"$2\n\"; exp_continue} \"RETURN\" {send \"\n\"; exp_continue} \"(yes/no)?\" {send \"yes\n\"; exp_continue} $3"
+# Installs expect if is not found in the environment
+install_expect() {
+    echo "Requested expect installtion..."
+    if [ "$GLOBAL_PLATFORM_OS" == "Darwin" ]; then
+        # Expect variables
+        readonly TCL_VERSION="8.6.9"
+        readonly EXPECT_VERSION="5.45.4"
+        # Download and install TCL
+        readonly TCL_FULL_NAME="tcl${TCL_VERSION}"
+        readonly TCL_PKG="${TCL_FULL_NAME}-src.tar.gz"
+
+        echo "Downloading dependency $TCL_PKG..."
+        # curl -sL -O http://downloads.sourceforge.net/tcl/tcl8.6.9-src.tar.gz --output tcl8.6.9-src.tar.gz
+        curl -sL -O http://downloads.sourceforge.net/tcl/$TCL_PKG --output $TCL_PKG
+        echo "Done!"
+        echo "Decompressing ..."
+        tar -xzf $TCL_PKG
+        echo "Done!"
+
+        echo "Compiling dependency $TCL_PKG..."
+        # Compilation and installation
+        make -C $TCL_FULL_NAME/macosx
+        make -C $TCL_FULL_NAME/macosx install INSTALL_ROOT="${HOME}/"
+        echo "Done!"
+
+        # Download and install Expect
+        readonly EXPECT_FULL_NAME="expect${EXPECT_VERSION}"
+        readonly EXPECT_PKG="${EXPECT_FULL_NAME}.tar.gz"
+        echo "Downloading expect $EXPECT_PKG..."
+        curl -sL -O https://downloads.sourceforge.net/expect/$EXPECT_PKG --output $EXPECT_PKG
+        echo "Done!"
+        echo "Decompressing ..."
+        tar -xzf $EXPECT_PKG
+        echo "Done!"
+
+        echo "Compiling $EXPECT_PKG..."
+        # Install
+        $EXPECT_FULL_NAME/configure --prefix=/usr \
+            --with-tcl=/usr/lib \
+            --enable-shared \
+            --mandir=/usr/share/man \
+            --with-tclinclude=/usr/include &&
+            make
+
+        echo "Installing expect..."
+        # Now, as the root user:
+        #sudo make $EXPECT_FULL_NAME/install &&
+        expectify "sudo ln -svf $EXPECT_FULL_NAME/libexpect5.45.4.so /usr/lib"
+        echo "Done!"
+    else
+        if [ "$GLOBAL_PLATFORM_OS" == "Linux" ]; then
+            echo "Installing expect..."
+            sudo apt-get install expect
+        fi
+    fi
+
+    return 0
 }
 
-expectify(){
-  if [ -z "$CURRENT_LOGON_PASSWORD" ]; then
-    read -s -p "Password (for $USER): " CURRENT_LOGON_PASSWORD
-    echo ""
-  fi
+# Check if expect is installed
+check_expect() {
+    echo "Checking for expect installation..."
+    if ! type expect >/dev/null 2>&1; then
+        echo "Not installed!"
+        return 1
+    fi
 
-  run_expect "$1" "$CURRENT_LOGON_PASSWORD" "$2"
+    echo "Installed!"
+    return 0
+}
+
+# Check if expect exists and if not, tries to install it automatically.
+check_install_expect() {
+    if ! check_expect; then
+        echo "Attempting to install automatically..."
+        install_expect
+
+        if [ $? -eq 0 ]; then
+            echo "Installation successfully..."
+        else
+            echo "Unable to install expect."
+            return 1
+        fi
+    fi
+
+    return 0
+}
+
+# Runs expect command to the supplied command, with the password and you can attach whatever rules you want.
+# $1 command
+# $2 password
+# $3 rules
+run_expect() {
+    check_install_expect
+
+    if [ $? -eq 0 ]; then
+        expect -c "set timeout -1; exp_internal 1; spawn $1; expect -re {(P|p)assword*} {send \"$2\n\"; exp_continue} \"RETURN\" {send \"\n\"; exp_continue} \"(yes/no)?\" {send \"yes\n\"; exp_continue} $3"
+    else
+        echo "Unable to use expect."
+        exit 1
+    fi
+}
+
+# Runs expect providing the CURRENT_LOGON_PASSWORD value as password.
+expectify() {
+    if [ -z "$CURRENT_LOGON_PASSWORD" ]; then
+        read -s -p "Password (for $USER): " CURRENT_LOGON_PASSWORD
+        echo ""
+    fi
+
+    run_expect "$1" "$CURRENT_LOGON_PASSWORD" "$2"
 }
