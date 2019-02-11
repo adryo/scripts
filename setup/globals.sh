492c2f43b2a2a87fcbb225f6d93e2d56b79e4221
adryo <adryoenterprise@gmail.com>
2019-02-11 13:06:48 -0200

    Update globals.sh

--- setup/globals.sh
+++ setup/globals.sh
@@ -7,7 +7,7 @@ CURRENT_LOGON_PASSWORD=""
 GLOBAL_PLATFORM=`uname`
 
 run_expect() {
-  expect -c "set timeout -1; spawn $1; expect \"*(p|P)assword*\" {send \"$2\n\"; exp_continue} \"RETURN\" {send \"\n\"; exp_continue} $3"
+  expect -c "set timeout -1; spawn $1; expect \"(P|p)assword*\" {send \"$2\n\"; exp_continue} \"RETURN\" {send \"\n\"; exp_continue} $3"
 }
 
 expectify(){
