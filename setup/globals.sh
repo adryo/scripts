d60513a4826ab980f3e089568986190984bda05f
adryo <adryoenterprise@gmail.com>
2019-02-12 09:47:02 -0200

    Update globals.sh

--- setup/globals.sh
+++ setup/globals.sh
@@ -60,7 +60,7 @@ install_expect() {
             sudo rm /var/lib/apt/lists/lock
             sudo rm /var/cache/apt/archives/lock
             sudo rm /var/lib/dpkg/lock
-            sudo apt install expect
+            sudo apt install expect -y
         fi
     fi
 
