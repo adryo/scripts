2a19a0917e5666285cbf5072839848cd6e18eeed
Yadir Hernandez Batista <yadirhb@gmail.com>
2019-02-20 11:35:28 -0300

    Update macos-vm.sh

--- setup/macos-vm.sh
+++ setup/macos-vm.sh
@@ -389,7 +389,7 @@ installVBox() {
   # Add user to vboxusers group
   expectify "sudo usermod -a -G vboxusers $USER"
 
-  expectify "sudo timeshift --create --comments 'Virtual Box installed'" #Create a restore point
+  expectify "sudo timeshift --create --comments \'Virtual\ Box\ installed\'" #Create a restore point
 }
 
 createVM() {
