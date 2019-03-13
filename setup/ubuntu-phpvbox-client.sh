8c0a38bf748c8eaa20f56da7639e044042312756
Yadir Hernandez Batista <yadirhb@gmail.com>
2019-03-13 12:43:23 -0300

    Update ubuntu-phpvbox-client.sh

--- setup/ubuntu-phpvbox-client.sh
+++ setup/ubuntu-phpvbox-client.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 PHPVBOX_VERSION="5.2-1"
+
 LOGON_PASSWORD=${CURRENT_LOGON_PASSWORD:-$1}
 
-echo "Starting PHP VBox Client v$PHPVBOX_VERSION installation"
+echo "Starting installation of PHP VBox Client v$PHPVBOX_VERSION"
 
 echo "Requesting to install dependencies..."
 sudo apt install unzip apache2 php7.2 php7.2-mysql libapache2-mod-php php7.2-xml -y
 
-if ! sudo apt install php7.2-soap; then
+if ! sudo apt install php7.2-soap -y; then
     wget http://security.ubuntu.com/ubuntu/pool/universe/p/php7.2/php7.2-soap_7.2.15-0ubuntu0.18.04.1_amd64.deb
-    if [ sudo dpkg -i php7.2-soap* ]; then
-        rm php7.2-soap*
-    fi
+    sudo dpkg -i php7.2-soap*
+    rm php7.2-soap*
 fi
 if [ "$?" -ne "0" ]; then
     echo "Unable to install dependencies. Stoping installation"
@@ -27,8 +27,11 @@ if wget https://github.com/phpvirtualbox/phpvirtualbox/archive/$PHPVBOX_PACKAGE 
     sudo chmod 777 -R /var/www/html/phpvirtualbox/
     echo "PHP VBox client installed!"
 
-    echo "Start configuring..."
+    echo "Configuring client ..."
+
     cp /var/www/html/phpvirtualbox/config.php-example /var/www/html/phpvirtualbox/config.php
+
+    echo "Installed default config file ..."
 
     sed -i -e "s/var \$username = 'vbox';/var \$username = '$USER';/g" /var/www/html/phpvirtualbox/config.php
 
@@ -38,17 +41,17 @@ if wget https://github.com/phpvirtualbox/phpvirtualbox/archive/$PHPVBOX_PACKAGE 
         sed -i -e "s/var \$password = 'pass';/var \$password = '$LOGON_PASSWORD';/g" /var/www/html/phpvirtualbox/config.php
     fi
 
+    echo "Setup credentials ..."
+
     sudo /bin/sh -c "echo VBOXWEB_USER=$USER >> /etc/default/virtualbox"
 
-    echo "Done!"
+    echo "Configured default user for vbox service ..."
 
     echo "Requesting services to restart..."
 
     sudo systemctl restart vboxweb-service
     sudo systemctl restart vboxdrv
     sudo systemctl restart apache2
-
-    echo "Done!"
 
     echo "Enabling configuration in firewall..."
     sudo ufw allow in "Apache Full"
