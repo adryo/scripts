b361b7c182c7778ae5505a95f451aaf36fe91ffe
Yadir Hernandez Batista <yadirhb@gmail.com>
2019-04-23 18:18:12 -0300

    Update install-nuget.ps1

--- install-nuget.ps1
+++ install-nuget.ps1
@@ -3,8 +3,8 @@ Param(
     [System.Version] $Version = "4.9.4"
 )
 try {
- FilePath="$env:BUILD_BINARIESDIRECTORY\nuget.exe"
- Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/v$Version/nuget.exe" -OutFile $FilePath
+ $FilePath = "${ENV:BUILD_BINARIESDIRECTORY}\nuget.exe"
+ Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/v$Version/nuget.exe" -OutFile "$FilePath"
  Write-Host "Nuget version: $Version, downloaded to '$FilePath'."
- Write-Host "##vso[task.setvariable variable=Path;]$env:BUILD_BINARIESDIRECTORY;$env:Path"
+ Write-Host "##vso[task.setvariable variable=Path;]${ENV:BUILD_BINARIESDIRECTORY};$env:Path"
 }catch {Write-Error -Message "Failed to download nuget.exe from $urlPrefix.  $($_.Exception.Message)" -ErrorAction Stop}
